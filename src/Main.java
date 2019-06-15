72a4b1bafa2d125068e473d98245c08ecdd03816
Damilola Randolph <damilolarandolph@gmail.com>
2019-06-15 05:50:52 +0000

    Reduced space complexity by using only two rows

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -16,9 +17,9 @@ public class Main {
 class LCS{
     private String template;
     private String compare;
-    private String[][] lcsTable;
-    private StringBuilder subsequence = new StringBuilder();
     private int length = 0;
+    private int[] row1;
+    private int[] row2;
     public LCS(File a, File b) throws IOException{
 
         StringBuilder container1 = new StringBuilder();
@@ -26,9 +27,7 @@ class LCS{
         while(reader.hasNextLine()){
             String line = reader.nextLine();
             for(int num = 0; num < line.length(); ++num){
-                if (line.charAt(num) != ' '){
                     container1.append(line.charAt(num));
-                }
             }
         }
 
@@ -38,9 +37,7 @@ class LCS{
         while(reader.hasNextLine()){
             String line = reader.nextLine();
             for(int num = 0; num < line.length(); ++num){
-                if (line.charAt(num) != ' '){
                     container2.append(line.charAt(num));
-                }
             }
         }
 
@@ -58,7 +55,7 @@ class LCS{
     }
 
     private void setLength(){
-        this.length = Integer.parseInt(this.lcsTable[this.lcsTable.length - 1][this.lcsTable[0].length - 1]);
+        this.length = this.row1[this.row1.length - 1];
     }
 
     public int getLength() {
@@ -71,74 +68,37 @@ class LCS{
 
     public void displayResults(){
         System.out.println("The length is: " + this.getLength());
-        double ratio = ((double)this.getLength()/this.compare.length()) * 100 ;
+        double ratio = (200 * (double)this.getLength()) /(this.compare.length() + this.template.length());
         System.out.printf("The plagiarism ratio is: %1.2f\n", ratio);
     }
 
-    public void displayBoard(){
-        for (String[] a: this.lcsTable){
-            for (String b: a){
-                System.out.print(b + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    private void getSubsequence(){
-        if (this.subsequence.length() == 0) {
-            int max = 0;
-            for (int a = 2; a < this.lcsTable[0].length; ++a) {
-                for (int b = 2; b < this.lcsTable.length; ++b) {
-                    if (Integer.parseInt(this.lcsTable[b][a]) > max) {
-                        this.subsequence.append(this.lcsTable[0][a]);
-                        max = Integer.parseInt(this.lcsTable[b][a]);
-                    }
-                }
-            }
-            System.out.println("The longest subsequence is: " + subsequence.toString());
-        }
-
-        else {
-            System.out.println("The longest subsequence is: " + subsequence.toString());
-        }
-    }
 
     public void search(){
-        for (int a = 2; a < this.lcsTable.length; ++a){
-            for (int b = 2; b < this.template.length() + 2 ; ++b){
-                if (!this.lcsTable[a][0].equals(this.lcsTable[0][b])){
-                      this.lcsTable[a][b] = Math.max(
-                              Integer.parseInt(this.lcsTable[a][b - 1]),
-                              Integer.parseInt(this.lcsTable[a - 1][b])
-                      ) + "";
+        for (int a = 0; a < this.compare.length(); ++a){
+            for (int b = 0; b < this.template.length(); ++b){
+                if (this.template.charAt(b) != this.compare.charAt(a)){
+                      this.row2[b + 1] = Math.max(
+                              this.row2[b],
+                              this.row1[b + 1]
+                      );
                 }
-
                 else{
-                    this.lcsTable[a][b] = "" +(Integer.parseInt(this.lcsTable[a - 1][b - 1]) + 1);
+                    this.row2[b + 1] = this.row1[b] + 1;
                 }
             }
+            int[] temp = this.row1;
+            Arrays.fill(temp, 0);
+            this.row1 = this.row2;
+            this.row2 = temp;
         }
+
     }
 
 
     private void initBoard(){
-        this.lcsTable = new String[this.compare.length() + 2][this.template.length() + 2];
-        this.lcsTable[0][0] = this.lcsTable[0][1] = "0";
-        for (int a = 0; a < this.lcsTable[0].length; ++a){
-                if (a  < this.template.length()){
-                    this.lcsTable[0][a + 2] = this.template.substring(a, a + 1);
-                }
-                this.lcsTable[1][a] = "0";
-        }
+        this.row1 = new int[this.template.length() + 1];
+        this.row2 = new int[this.template.length() + 1];
 
-        for (int a = 2; a < this.lcsTable.length; ++a){
-            if (a - 2 < this.compare.length()){
-                this.lcsTable[a][0] = this.compare.substring(a - 2, a - 1);
-                this.lcsTable[a][1] = "0";
-            }
-
-        }
     }
 
 
